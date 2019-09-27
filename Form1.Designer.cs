31de67583a0dbcebd6ddba6eb9350f405341d8f2
Sharmin-dishari <sharminakter1820@gmail.com>
2019-09-28 00:07:08 +0600

    CRUD

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Coffe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(133, 96);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(133, 51);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(1175, -147);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(451, 54);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(303, 194);
+            this.DataGridView.TabIndex = 3;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(133, 138);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address";
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(133, 175);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(100, 20);
+            this.contactTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contact";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(133, 253);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 8;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(214, 253);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // delButton
+            // 
+            this.delButton.Location = new System.Drawing.Point(295, 253);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(75, 23);
+            this.delButton.TabIndex = 10;
+            this.delButton.Text = "Delete";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(376, 253);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 11;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(457, 253);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 12;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 311);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Connection
+                string connectionString = @"Server=DESKTOP-VJNJ93K\; DataBase=COFFESHOP; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command
+
+                string commandString = @"SELECT * FROM Customer";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Execute
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    DataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    DataGridView.DataSource = null;
+                    MessageBox.Show("No Data Found");
+                }
+
+
+                //Close
+                sqlConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something is Wrong");
+            }
+
+        
+    }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //Connection
+                string connectionString = @"Server=DESKTOP-VJNJ93K\; DataBase=COFFESHOP; Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Command
+                //INSERT INTO Items (Name, Price) Values ('Black', 120)
+                string commandString = @"INSERT INTO Customer (Name, Address,Contact) Values ('" + nameTextBox.Text + "', '" + addressTextBox.Text + "'," + contactTextBox.Text + ")";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                //Open
+                sqlConnection.Open();
+
+                //Execute
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("Saved");
+                }
+                else
+                {
+                    MessageBox.Show("Not Saved");
+                }
+
+                //Close
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+        
+    }
+
+        private void delButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+            string connectionString = @"Server=DESKTOP-VJNJ93K\; DataBase=COFFESHOP; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+            //DELETE FROM Items WHERE ID = 3
+            string commandString = @"DELETE FROM Customer WHERE Id = " + idTextBox.Text + "";
+            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+            //Open
+            sqlConnection.Open();
+
+            //Execute
+            int isExecuted = sqlCommand.ExecuteNonQuery();
+
+            if (isExecuted > 0)
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+
+            //Close
+            sqlConnection.Close();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            // SQL connection
+            string connectionString = @"Server=DESKTOP-VJNJ93K\; DataBase=COFFESHOP; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            //Sql Command
+
+            string commandString = "SELECT * FROM Customer WHERE Id =" + idTextBox.Text + "";
+            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+            sqlConnection.Open();
+            //Execute
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                MessageBox.Show("Search Match");
+                DataGridView.DataSource = dataTable;
+
+            }
+            else
+            {
+                MessageBox.Show("Search Data Not Match");
+            }
+            sqlConnection.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            // SQL connection 
+            string connectionString = @"Server=DESKTOP-VJNJ93K\; DataBase=COFFESHOP; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            //Sql Command
+
+            string commandString = @"UPDATE Customer SET Name ='" + nameTextBox.Text + "', Address ='" + addressTextBox.Text + "', Contact = " + contactTextBox.Text + " WHERE Id =" + idTextBox.Text + "";
+            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+            sqlConnection.Open();
+            int isExecute = sqlCommand.ExecuteNonQuery();
+            if (isExecute > 0)
+            {
+                MessageBox.Show("Successfully Updated");
+            }
+            else
+            {
+                MessageBox.Show("Updated Failed");
+            }
+
+            sqlConnection.Close();
+        }
+    }
+    
+}
